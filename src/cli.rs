462ad88cfdf98b335c282c44004e32dc5015419e
derpz-discord <124417811+derpz-discord@users.noreply.github.com>
2023-02-23 00:06:12 +0800

    ok this seems to compile

---
+++ src/cli.rs
@@ -0,0 +1,38 @@
+use std::path::{Path, PathBuf};
+use clap::{ArgAction, Parser, ValueEnum};
+use serde::Serialize;
+
+#[derive(Copy, Clone, Debug, Eq, Ord, PartialEq, PartialOrd, ValueEnum, Serialize)]
+pub enum Environment {
+    /// Builds a preamble for use with Overleaf
+    Overleaf,
+    /// Builds a preamble for use with TeXit
+    Texit,
+}
+
+// add to string for env
+impl Into<String> for Environment {
+    fn into(self) -> String {
+        match self {
+            Environment::Overleaf => "overleaf".to_string(),
+            Environment::Texit => "texit".to_string(),
+        }
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+pub struct CliArgs {
+    /// Increase the level of tracing/logging
+    #[arg(short, long, action = ArgAction::Count)]
+    pub verbosity: u8,
+
+    /// Which environment we are building the preamble for.
+    #[arg(value_enum, default_value = "texit")]
+    pub environment: Environment,
+
+    /// The config file to read out of
+    #[arg(short, long, default_value = "stitchconfig.yml", value_parser = clap::value_parser!(PathBuf))]
+    pub config_path: PathBuf,
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,53 @@
+use clap::Parser;
+use preamble_stitch::cli::CliArgs;
 use std::path::Path;
+use tracing::{debug, error, info};
+use preamble_stitch::{get_config_from_fs, read_files, stitch_and_render, write_file};
 
 fn main() {
-    let config = preamble_stitch::get_config_from_fs(Path::new("stitchconfig.yml")).unwrap();
-    let files = preamble_stitch::read_files(&config.order).unwrap();
-    let stitched = preamble_stitch::stitch_files(&files);
+    // setup tracing with env filter
+    let cli_opts = CliArgs::parse();
+    tracing_subscriber::fmt()
+        .with_max_level(match cli_opts.verbosity {
+            0 => tracing::Level::INFO,
+            1 => tracing::Level::DEBUG,
+            _ => tracing::Level::TRACE,
+        })
+        .init();
+
+
+    info!("Building for environment {:?}", cli_opts.environment);
+    debug!("CLI Options: {:?}", cli_opts);
+    let config = get_config_from_fs(Path::new(&cli_opts.config_path));
+    if let Err(e) = config {
+        error!("Error reading config file: {}", e);
+        std::process::exit(1);
+    }
+    let config = config.unwrap();
+
+    let files = read_files(&config.order);
+    if let Err(e) = files {
+        error!("Error reading files: {}", e);
+        std::process::exit(1);
+    }
+    let files = files.unwrap();
+    let _ = files.iter().map(|file| info!("+ {}", file.path.display()));
+    let stitched = stitch_and_render(&files, cli_opts.environment);
+    if let Err(e) = stitched {
+        error!("Error stitching files: {}", e);
+        std::process::exit(1);
+    }
+    let stitched = stitched.unwrap();
     let output_file = match &config.output_file {
         Some(path) => path,
         None => Path::new("output.sty"),
     };
-    preamble_stitch::write_file(stitched, output_file).unwrap();
+    info!("Done, now writing to `{}`", output_file.display());
+    let write_status = write_file(stitched, output_file);
+    if let Err(e) = write_status {
+        error!("Error writing file: {}", e);
+        std::process::exit(1);
+    }
+    info!("Done!");
+
 }
